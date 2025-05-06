7c5fb1c71cd51d3cb851f4adad3673e0ebc8b04e
Milk-Cool <coder.tomsk@gmail.com>
2025-05-06 23:03:10 +0300

    v1

---
+++ cli.js
@@ -0,0 +1,88 @@
+#!/bin/env node
+import Spider8831 from "./index.js";
+import { parseArgs } from "util";
+import { readFileSync, appendFileSync, writeFileSync, mkdirSync, existsSync } from "fs";
+import { join } from "path";
+
+const { values, positionals } = parseArgs({
+    allowPositionals: true,
+    options: {
+        help: { type: "boolean", "short": "h" },
+        input: { type: "string", "short": "i", multiple: true },
+        output: { type: "string", "short": "o" },
+        silent: { type: "boolean", "short": "s" },
+        width: { type: "string", "short": "W" },
+        height: { type: "string", "short": "H" },
+        follow: { type: "string", "short": "F" },
+        depth: { type: "string", "short": "D" },
+    }
+});
+
+if(values.help) {
+    console.log(`
+Spider8831 usage:
+
+spider8831 [-hs] [-i INPUT_FILE] [-o OUTPUT_DIR] [-W WIDTH] [-H HEIGHT] [-F FOLLOW] [-D DEPTH] [...websites]
+TODO for devs
+`);
+    process.exit(0);
+}
+
+/** @type {import("./index.js").Spider8831Link[]} */
+const urls = positionals
+    .concat(values.input ? values.input.map(x => readFileSync(x, "utf-8").split("\n").filter(Boolean)).flat() : [])
+    .map(x => ({ url: x, depth: 0 }));
+if(urls.length === 0) {
+    console.log("No URLs provided.");
+    process.exit(1);
+}
+
+/** @type {import("./index.js").Spider8831Options} */
+const opts = {};
+if(values.depth) opts.depth = parseInt(values.depth);
+if(values.follow) opts.follow = new RegExp(values.follow);
+if(values.width) opts.width = parseInt(values.width);
+if(values.height) opts.height = parseInt(values.height);
+const spider = new Spider8831(opts);
+
+/** @param {import("./index.js").Spider8831CallbackOpts} data */
+const cb = (data) => {
+    if(values.silent) return;
+    console.log({
+        image: "\x1b[32m\x1b[1mIMAGE\x1b[0m",
+        link: "\x1b[34m\x1b[1mLINK\x1b[0m",
+        error: "\x1b[31m\x1b[1mERROR\x1b[0m"
+    }[data.type] + ": " + data.url);
+};
+
+if(values.output && !existsSync(values.output)) {
+    mkdirSync(values.output);
+}
+if(values.output) {
+    writeFileSync(join(values.output, "index.txt"), "");
+}
+let out = [], n = 0;
+
+(async () => {
+    while(urls.length > 0) {
+        const url = urls[0];
+        const res = await spider.scan(url.url, url.depth, cb);
+        if(res.imgs.length !== 0 || res.next.length !== 0) console.log(`Scanned ${url.url} (depth ${url.depth})`);
+        for(const img of res.imgs) {
+            out.push(img.url);
+            if(values.output) {
+                writeFileSync(join(values.output, n + img.url.slice(img.url.lastIndexOf("."))), img.img);
+                appendFileSync(join(values.output, "index.txt"), `${n}: ${img.url}\n`);
+                n++;
+            }
+        }
+        for(const url of res.next) {
+            urls.push(url);
+        }
+        urls.splice(0, 1);
+    }
+
+    for(const outURL of out) {
+        console.log(outURL);
+    }
+})();
---
+++ index.js
@@ -0,0 +1,158 @@
+import { JSDOM } from "jsdom";
+import { imageSize } from "image-size";
+
+/**
+ * @typedef {object} Spider8831Options
+ * @property {number} [width]
+ * @property {number} [height]
+ * @property {RegExp | (url: string) => boolean | false} [follow]
+ * @property {number} [depth]
+ */
+/**
+ * @typedef {object} Spider8831CallbackOpts
+ * @property {"image" | "link" | "error"} type
+ * @property {string} url
+ */
+/**
+ * @typedef {object} Spider8831Link
+ * @property {string} url
+ * @property {number} depth
+ */
+/**
+ * @typedef {object} Spider8831Image
+ * @property {string} url
+ * @property {Buffer} img
+ */
+/**
+ * @typedef {object} Spider8831Return
+ * @property {Spider8831Link[]} next
+ * @property {Spider8831Image[]} imgs
+ */
+
+export default class Spider8831 {
+    static defaultFollow = /^https?:\/\/.+\.neocities\.org/;
+
+    // /**
+    //  * @param {string} url
+    //  * @returns {boolean}
+    //  */
+    // static imgURL(url) {
+    //     const urlObj = new URL(url);
+    //     return urlObj.pathname.match(/\.(png|jpe?g|gif|webp|bmp)$/);
+    // }
+
+    /**
+     * @param {Spider8831Options} options 
+     */
+    constructor(options) {
+        this.width = options.width ?? 88;
+        this.height = options.height ?? 31;
+        this.follow = options.follow ?? (() => Spider8831.defaultFollow);
+        this.depth = options.depth ?? 5;
+
+        /** @type {string[]} */
+        this.visited = [];
+    }
+
+    /**
+     * @param {string | URL} url
+     * @returns {Promise<JSDOM>}
+     */
+    static async fetchJSDOM(url) {
+        const f = await fetch(url);
+        const t = await f.text();
+        const dom = new JSDOM(t, { contentType: "text/html" });
+        return dom;
+    }
+
+    /**
+     * @param {string | URL} url 
+     * @returns {Promise<Buffer>}
+     */
+    static async fetchImage(url) {
+        const f = await fetch(url);
+        const b = Buffer.from(await f.arrayBuffer());
+        return b;
+    }
+
+    /**
+     * @param {Buffer} buf 
+     * @returns {[number,  number]}
+     */
+    static getImageSize(buf) {
+        const dim = imageSize(buf);
+        return [dim.width, dim.height];
+    }
+
+    /**
+     * @param {string | URL}
+     * @returns {string}
+     */
+    static getURL(url) {
+        if(url instanceof URL) url = url.href;
+        return url;
+    }
+
+    /**
+     * @param {string}
+     * @returns {boolean}
+     */
+    checkURL(url) {
+        return !this.visited.includes(url)
+            && (this.follow instanceof RegExp ? !!url.match(this.follow) : this.follow(url));
+    }
+
+    /**
+     * Scans a single page recursively.
+     * @param {string | URL} url
+     * @param {number} [depth]
+     * @param {(opts: Spider8831CallbackOpts) => void} [cb]
+     * @returns {Promise<Spider8831Return>}
+     */
+    async scan(url, depth = 0, cb = (_opts) => {}) {
+        if(depth >= this.depth) return { next: [], imgs: [] };
+        url = Spider8831.getURL(url);
+        this.visited.push(url);
+        /** @type {JSDOM} */
+        let dom;
+        try {
+            dom = await Spider8831.fetchJSDOM(url);
+        } catch(_) {
+            cb?.({ type: "error", url: url });
+            return { next: [], imgs: [] };
+        }
+        const { document } = dom.window;
+        const links = Array.from(document.querySelectorAll("a"));
+        const images = Array.from(document.querySelectorAll("img"));
+        
+        /** @type {Spider8831Link[]} */
+        const next = [];
+        /** @type {Spider8831Image[]} */
+        const imgs = [];
+
+        for(const link of links) {
+            let href = link.href;
+            if(!href) continue;
+            href = new URL(href, new URL(url).origin).href;
+            if(!this.checkURL(href)) continue;
+            cb?.({ type: "link", url: href });
+            next.push({ depth: depth + 1, url: href });
+        }
+        for(const img of images) {
+            let src = img.src;
+            if(!src) continue;
+            src = new URL(src, new URL(url).origin).href;
+            this.visited.push(src);
+            try {
+                const img = await Spider8831.fetchImage(src);
+                const size = Spider8831.getImageSize(img);
+                if(size[0] !== this.width || size[1] !== this.height) continue;
+                cb?.({ type: "image", url: src });
+                imgs.push({ img, url: src });
+            } catch(_) {
+                cb?.({ type: "error", url: src });
+            }
+        }
+        return { imgs, next };
+    }
+}
